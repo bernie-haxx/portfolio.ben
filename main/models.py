283e0fd30f4b31f015d1e5cdb806f43ff05b55b4
Benard18 <benkaranja43@gmail.com>
2018-08-12 18:18:16 +0300

    this is my project class that will hold my projects based on my portfolio

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+	name = models.CharField(max_length=100, null=True, blank=True)
+	description=models.CharField(max_length=200, null=True, blank=True)
+	picture = models.ImageField(upload_to='images/', blank=True,default="/black.png")
+	url = models.CharField(max_length=100, null=True, blank=True)
+	
+
+	def __str__(self):
+		return self.name
+
