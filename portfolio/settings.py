0b92d8828be121c1e4e9a1fbd79b0d2924f73ce0
Benard18 <benkaranja43@gmail.com>
2018-08-12 18:20:23 +0300

    removed the db.sqlite3 database and replaced it with the postgresql database in conjuction with the psycopg2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,10 +76,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'portfolio',
+           'USER': 'ben',
+           'PASSWORD': 'ben',
+       }
+
 }
 
 
