263dbb3907fe4fee6e57fa29b3567ae9fc71cd4e
Benard18 <benkaranja43@gmail.com>
2018-08-30 08:06:18 +0300

    these are the urls based on the application

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns=[
 url(r'^$',views.home,name="home"),
+url(r'^about/$',views.about,name="about"),
 ]
+
+if settings.DEBUG:
+	urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
