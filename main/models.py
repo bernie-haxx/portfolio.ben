7491fdbdbd74222f782879144c6e400e32463119
Benard18 <benkaranja43@gmail.com>
2018-08-30 08:04:14 +0300

    functions to the category class

--- main/models.py
+++ main/models.py
@@ -8,10 +8,12 @@ class Category(models.Model):
 
 class Project(models.Model):
 	name = models.CharField(max_length=100, null=True, blank=True)
-	description=models.CharField(max_length=200, null=True, blank=True)
+	description=models.CharField(max_length=300, null=True, blank=True)
+	role=models.CharField(max_length=100, null=True, blank=True)
+	Technologies=models.CharField(max_length=300, null=True, blank=True)
 	picture = models.ImageField(upload_to='images/', blank=True,default="/black.png")
 	url = models.CharField(max_length=100, null=True, blank=True)
-	
+	category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name="category",null=True,blank=True)	
 
 	def __str__(self):
 		return self.name
