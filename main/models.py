a6d9f691c046b1101e96ff898d5263282d34da7f
Benard18 <benkaranja43@gmail.com>
2018-08-30 08:03:51 +0300

    adding the category class to the models

--- main/models.py
+++ main/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=100, null=True, blank=True)
+
+	def __str__(self):
+		return self.name
+
 class Project(models.Model):
 	name = models.CharField(max_length=100, null=True, blank=True)
 	description=models.CharField(max_length=200, null=True, blank=True)
