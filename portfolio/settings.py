cef6f8a10185e87f7cae8a0e832a9efa06eafd3c
Benard18 <benkaranja43@gmail.com>
2018-09-21 10:12:58 +0300

    adding changes to the settings file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+MODE="dev"
+SECRET_KEY = 'wewewewe'
+DEBUG = config('DEBUG', default=True, cast=bool)
 # development
-if config('MODE')=="dev":
+if MODE == "dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
+           'NAME': 'portfolio',
+           'USER': 'postgres',
+           'PASSWORD': 'ben',
+           'HOST': '',
            'PORT': '',
        }
        
@@ -103,7 +103,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ''
 
 
 # Password validation
