a5b296f5708730dd830ba0e5d53de53159d88fc5
Benard18 <benkaranja43@gmail.com>
2018-10-17 22:34:01 +0300

    have only one url at the moment...be patient

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+urlpatterns = [
+	url(r'^$',views.home,name="home")
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
